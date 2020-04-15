3b25a54a8a5962258d933101ad213c6ad8a9c715
Stephen Montgomery <stephen.montgomery@sievo.com>
2020-04-15 15:28:29 +0300

    react app with average fp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import _ from "lodash";
 import logo from './logo.svg';
 import './App.css';
+
+const enrollment = [
+    { enrolled: 2, grade: 100 },
+    { enrolled: 2, grade: 80 },
+    { enrolled: 1, grade: 89 }
+  ];
+  
+  let enrolled = enrollment.filter(s => s.enrolled > 1);
+  let grades = enrolled.map(e => e.grade);
+  let sum = grades.reduce((a, b) => a + b, 0);
+  let total = grades.length;
+  let mean = sum / total;
+  console.log(mean);
+  
+  const output = _.chain(enrollment)
+    .filter(s => s.enrolled > 1)
+    .map("grade")
+    .mean()
+    .value();
+  
+  console.log(output);
+  
+  const output1 = _.meanBy(enrollment, "grade");
+  
+  console.log(output1);
 
 function App() {
   return (
@@ -10,14 +36,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
