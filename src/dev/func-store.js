eb60fdba0e7adce8e5ad0e99a65145f0d9cab2a9
Stephen Montgomery <stevemonty@yahoo.com>
2020-04-19 16:44:27 +0300

    Simple assert func store

---
+++ src/dev/func-store.js
@@ -0,0 +1,18 @@
+import { assert } from "./assert.js";
+
+var ninja = {};
+
+var store = {
+  nextId: 1,
+  cache: {},
+  add: function (fn) {
+    if (!fn.id) {
+      fn.id = this.nextId++;
+      this.cache[fn.id] = fn;
+      return true;
+    }
+  },
+};
+
+assert(store.add(ninja), "Function was safely added.");
+assert(!store.add(ninja), "But it was only added once.");
--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
 /* eslint-disable no-unused-vars */
-// import {mean, output, output1} from "./dev/Enrollment.js";
-// import { message, sayHiToNinja} from "./dev/Ninja.js";
-import * as arrayExamples from "./dev/array.js";
-// import Person from './model/Person.js';
-import Student from "./model/Student.js";
-import Address from "./model/Address.js";
 
-// var curry = new Student('Haskell', 'Curry', '111-11-1111', 'Penn State');
-// curry.address = new Address('US');
+import { funcStore } from "./dev/func-store.js";
 
-// var turing = new Student('Alan', 'Turing', '222-22-2222', 'Princeton');
-// turing.address = new Address('England');
-
-// var church = new Student('Alonzo', 'Church', '333-33-3333', 'Princeton');
-// church.address = new Address('US');
-
-// var kleene = new Student('Stephen', 'Kleene', '444-44-4444', 'Princeton');
-// kleene.address = new Address('US');
