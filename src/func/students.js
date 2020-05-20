d111abe341537d1cb19517ca2e3e68f3b5313482
Stephen Montgomery <stephen.montgomery@sievo.com>
2020-05-20 15:16:00 +0300

    Fix merge paths

--- src/func/students.js
+++ src/func/students.js
@@ -1,18 +1,18 @@
 // import _ from 'lodash';
 // eslint-disable-next-line no-unused-vars
-import { assert, report, printMessage } from '../ninja/simple-test';
+import { assert, report, printMessage } from '../test-simple/simple-test';
 import Student from './model/Student';
 import Person from './model/Person';
 import Address from './model/Address';
 
-const curry = new Student("Haskell", "Curry", "111-11-1111", "Penn State");
-curry.address = new Address("US");
+const curry = new Student('Haskell', 'Curry', '111-11-1111', 'Penn State');
+curry.address = new Address('US');
 
-const turing = new Student("Alan", "Turing", "222-22-2222", "Princeton");
-turing.address = new Address("England");
+const turing = new Student('Alan', 'Turing', '222-22-2222', 'Princeton');
+turing.address = new Address('England');
 
-const church = new Student("Alonzo", "Church", "333-33-3333", "Princeton");
-church.address = new Address("US");
+const church = new Student('Alonzo', 'Church', '333-33-3333', 'Princeton');
+church.address = new Address('US');
 
 const kleene = new Student('Stephen', 'Kleene', '444-44-4444', 'Princeton');
 kleene.address = new Address('US');
@@ -23,12 +23,11 @@ report(
     .map((s) => s.fullname),
 );
 
-//imperative
-report('studentsInSameCountryAndSchool: ' + church.studentsInSameCountryAndSchool([curry, turing, kleene]).map((s) => s.fullname));
+// imperative
+report(`studentsInSameCountryAndSchool: ${church.studentsInSameCountryAndSchool([curry, turing, kleene]).map((s) => s.fullname)}`);
 
-//func
+// func
 const selector = (country, school) => (student) => student.address.country === country && student.school === school;
-const findStudentsBy = (friends, fn) => friends.filter(fn);
 
 const findStudentsBy = (friends, selectorfn) => friends.filter(selectorfn);
 
