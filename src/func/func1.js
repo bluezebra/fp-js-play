0c075f90041c9d39745944c47f14d2257a0a36fb
Stephen Montgomery <steve@Sievo118.sievofi.local>
2020-05-24 21:04:04 +0300

    minor

--- src/func/func1.js
+++ src/func/func1.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { assert, report, printMessage } from '../ninja/simple-test';
+import { assert, report, printMessage } from '../test-simple/simple-test';
 
 const increment = (i) => i + 1;
 
