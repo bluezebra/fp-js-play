bcc2b05dd02b300835a3d5b1bdd57adcbbd9e8cb
Stephen Montgomery <stephen.montgomery@sievo.com>
2020-06-24 16:24:57 +0300

    debug cats

--- src/mostly-adequate/flickr.js
+++ src/mostly-adequate/flickr.js
@@ -13,19 +13,20 @@ const path = '/services/feeds/photos_public.gne';
 const query = t => `?tags=${t}&format=json&jsoncallback=?`;
 const url = t => `https://${host}${path}${query(t)}`;
 
-// const app = R.compose(Impure.getJSON(Impure.trace('response')), url);
+const appTrace = R.compose(Impure.getJSON(Impure.trace('response')), url);
+appTrace('cats');
 
 const mediaUrl = R.compose(R.prop('m'), R.prop('media'));
 const mediaUrls = R.compose(R.map(mediaUrl), R.prop('items'));
 
-// const render = R.compose(Impure.setHtml('#js-main'), mediaUrls);
-// const app = R.compose(Impure.getJSON(render), url);
+const renderPageTrace = R.compose(Impure.setHtml('#playground'), mediaUrls);
+const appRenderPageTrace = R.compose(Impure.getJSON(renderPageTrace), url);
+appRenderPageTrace('cats');
 
 const img = src => $('<img />', { src });
 
 const images = R.compose(R.map(img), mediaUrls);
 const render = R.compose(Impure.setHtml('#js-main'), images);
 const app = R.compose(Impure.getJSON(render), url);
-
 
 app('cats');
