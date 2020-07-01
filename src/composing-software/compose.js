5110c41c81ae481dcf2d1590fbaa43c655f1859c
Stephen Montgomery <stevemonty@yahoo.com>
2020-07-01 14:11:16 +0300

    pipe eric

--- src/composing-software/compose.js
+++ src/composing-software/compose.js
@@ -1,15 +1,30 @@
+import _ from 'lodash';
+import * as R from 'ramda';
 // eslint-disable-next-line no-unused-vars
-import { assert, report, printMessage } from '../test-simple/simple-test';
+import { assert, report, printMessage } from '../test-simple/simple-test'
+
+const g = n => n + 1
+const f = n => n * 2
+// const doStuffBetter = x => f(g(x));
 
 
 const trace = label => value => {
-  console.log(`${label}: ${value}`);
-  return value;
-};
+  console.log(`${label}: ${value}`)
+  return value
+}
 
 
-const g = n => n + 1;
-const f = n => n * 2;
-const doStuffBetter = x => f(g(x));
+// const traceG = trace('after g')
+// const traceF = trace('after f')
+// const doStuffBetter = x => traceF(f(traceG(g(x))))
 
-report(doStuffBetter(20)); // 42
+
+const doStuffBetter = R.pipe(
+  g,
+  trace('after g'),
+  f,
+  trace('after f')
+);
+
+
+report(doStuffBetter(20)) // 42
